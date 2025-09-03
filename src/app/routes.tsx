21ab5d714b75a16bd10dd40273b164b78ac3ffb3
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-09-03 14:39:42 +0200

    refactor(navigation.tsx, routes.tsx): create component Navigation and Layout for routes

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,17 +2,20 @@ import { PostsPage } from '../features/posts/pages/PostsPage';
 import { PostDetailPage } from '../features/posts/pages/PostDetailPage';
 import { CreatePostPage } from '../features/posts/pages/CreatePostPage';
 import { Routes, Route } from 'react-router-dom';
+import { Layout } from '../shared/components/Layout';
 
 const NoMatch = () => <p>Error 404. Page Not Found</p>
 
 export default function AppRouter() {
     return (
-      <Routes>
-        <Route path="/" element={<PostsPage />} />
-        <Route path="/post/:id" element={<PostDetailPage />} />
-        <Route path="/create" element={<CreatePostPage />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route index element={<PostsPage />} />
+                <Route path="post/:id" element={<PostDetailPage />} />
+                <Route path="create" element={<CreatePostPage />} />
+                <Route path="*" element={<NoMatch />} />
+            </Route>
+        </Routes>
     );
   }
 
--- src/shared/components/Navigation.tsx
+++ src/shared/components/Navigation.tsx
@@ -1,6 +1,21 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export function Navigation() {
-    return <nav>Navigation</nav>;
+    const style = ({ isActive }: { isActive: boolean }) => ({
+        fontWeight: isActive ? "bold" : "normal",
+        textDecoration: "none",
+    });
+
+    //TODO internalization (i18n)
+    return (
+        <nav style={{ marginBottom: "1rem" }}>
+            <NavLink to="/" style={style}>
+                Posts
+            </NavLink>{" "}
+            |{" "}
+            <NavLink to="/create" style={style}>
+                Create
+            </NavLink>
+        </nav>
+    );
 }
