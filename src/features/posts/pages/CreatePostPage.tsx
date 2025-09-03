93d851df7d41ca4ee306068ea28d3ccbfb3d6b1f
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-09-03 16:44:38 +0200

    feat(createpostpage, postdetailpage): add feature create a new post

--- src/features/posts/pages/CreatePostPage.tsx
+++ src/features/posts/pages/CreatePostPage.tsx
@@ -1,5 +1,35 @@
-// import React, { useState } from 'react';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import type { PostFormValues } from "../components/PostForm";
+import { PostForm } from "../components/PostForm";
+import { createPost } from "../services/postApi";
 
-export function CreatePostPage() {
-    return <div>CreatePostPage</div>;
+function CreatePostPage() {
+    const navigate = useNavigate();
+    const [serverError, setServerError] = useState("");
+
+    const handleCreate = async (values: PostFormValues) => {
+        const res = await createPost(values);
+        setServerError("");
+        try {
+            if (res.status === 201 && res.data?.id) {
+                console.log("✅ Post created successfully:", res.data);
+                await createPost(values);
+                navigate("/");
+            }
+        } catch (e: any) {
+            setServerError(e?.message ?? "Server error.");
+            throw e;
+        }
+    };
+
+    return (
+        <>
+            <h2 className="text-xl font-semibold mb-4">Create Post</h2>
+            {serverError && <p className="text-red-600 mb-3">{serverError}</p>}
+            <PostForm onSubmit={handleCreate} submitLabel="Create" />
+        </>
+    );
 }
+
+export { CreatePostPage };
--- src/features/posts/pages/PostDetailPage.tsx
+++ src/features/posts/pages/PostDetailPage.tsx
@@ -1,5 +1,10 @@
 // import React, { useState, useEffect } from 'react';
 
 export function PostDetailPage() {
-    return <div>PostDetailPage</div>;
+
+    return (
+        <>
+
+        </>
+    );
 }
